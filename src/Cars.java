640a2652a3b5cc3c3a0aee7158b824c7d7334768
Marcin Konieczny <marcin_k@coni.pl>
2017-09-15 16:23:53 +0200

    Version 1.1

--- src/Cars.java
+++ src/Cars.java
@@ -1,7 +1,7 @@
 class Cars {
     public static void main(String[] args) {
         Car car1 = new Car("BMW", "M5", 2007, 2.6); //marka, model, rocznik, pojemność silnika
-        Car car2 = car1;
+        Car car2 = new Car("Opel", "Vectra", 2004, 1.8);
 
         System.out.println("Samochód 1:");
         System.out.println("Marka: " + car1.brand + ", model: " + car1.model + ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
